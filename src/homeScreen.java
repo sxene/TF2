a62349e1fa91c3d998127c61b726e0413a8caee8
sxene <zoey.a.desaulniers@gmail.com>
2025-11-13 12:09:09 -0500

    Login system in Works

--- src/homeScreen.java
+++ src/homeScreen.java
@@ -5,6 +5,10 @@ import java.awt.event.ActionListener;
 
 
 public class homeScreen extends JLabel implements ActionListener {
+    boolean login;
+    String username;
+    String password;
+    JLabel userBox;
     public homeScreen() {
         ImageIcon backgroundImage = new ImageIcon("/home/zoey/IdeaProjects/TF2/src/Images/hs1.jpg");
         setIcon(backgroundImage);
@@ -12,6 +16,26 @@ public class homeScreen extends JLabel implements ActionListener {
 
         int level;
         ImageIcon levelImage = new ImageIcon("/home/zoey/IdeaProjects/TF2/src/Images/jpg");
+        userBox = new JLabel();
+        userBox.setBounds(75, 0, 400, 250);
+        userBox.setFont(new Font("Arial", Font.BOLD, 100));
+        userBox.setText("TTTTT");
+        userBox.setForeground(Color.BLACK);
+
+
+        if (username != null) {
+            login = true;
+        }
+        else {
+            login = false;
+        }
+        if (!login) {
+
+
+        }
+
+        add(userBox);
+
 
 
     }
--- src/optionsMenu.java
+++ src/optionsMenu.java
@@ -4,10 +4,24 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class optionsMenu extends JLabel implements ActionListener {
+        JButton back;
         optionsMenu(){
             setBackground(Color.BLACK);
             setForeground(Color.WHITE);
             setVisible(true);
+            back = new JButton("Back");
+            back.setBounds(50, 50, 341, 70);
+            back.addActionListener(e -> {
+                System.out.println("doggnottt");
+                JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(this);
+                frame.remove(this);
+                startScreen oM = new startScreen();
+                frame.add(oM);
+                frame.revalidate();
+                frame.repaint();
+            });
+            add(back);
+
 
         }
 
